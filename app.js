1b3582ed942684cbc86720eb7fe63f5086d8e30e
josepserra20 <jserravives@cifpfbmoll.eu>
2023-01-06 17:18:57 +0100

    feat: :zap: app and server

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
 //Cargar rutas
 const hello_routes = require("./routes/hello");
+const task_routes = require("./routes/task");
+const bike_routes = require("./routes/bike");
 
 //Ruta base
 app.use("/api", hello_routes);
+app.use("/api", task_routes);
+app.use("/api", bike_routes);
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const app = require("./app");
-const port = 3000;
+const mongoDB = require("./DB/connect");
 
-app.listen(port, () => {
-    console.log("Servidor del API REST esta funcionando en localhost")
-}); asd
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+let environment = process.env.NODE_ENV || "development";
+
+app.listen(port, () => { 
+    console.log(`Environment: ${environment} example app listening at http://localhost:${port}/api/hello 🎅`);
+  });
+
+// conexion mongoDB
+mongoDB.connect();
+
+
+
+
+  
+  
+  
