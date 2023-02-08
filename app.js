1715578e757c904fcbbdec8e78a855226a74342e
josepserra20 <jserravives@cifpfbmoll.eu>
2023-02-08 17:12:46 +0100

    feat: add routes
    add new routes store stock

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ app.use(express.urlencoded({ extended: true }));
 
 //Cargar rutas
 const hello_routes = require("./routes/hello");
-const task_routes = require("./routes/task");
 const bike_routes = require("./routes/bike");
+const store_routes = require ("./routes/store")
+const stock_routes = require ("./routes/stock")
 
 //Ruta base
 app.use("/api", hello_routes);
-app.use("/api", task_routes);
 app.use("/api", bike_routes);
+app.use("/api", store_routes)
+app.use("/api", stock_routes)
 
 module.exports = app;
